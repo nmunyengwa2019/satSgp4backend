6c6a3c9e79bef190bcf847959c945adc2e43e834
nmunyengwa2019@gmail.com <nmunyengwa2019@gmail.com>
2024-01-22 16:37:57 +0200

    added app.run

--- flask_sgp4.py
+++ flask_sgp4.py
@@ -82,3 +82,6 @@ def sgp4(satelieName, fistline, secondline):
         return jsonify(position)
             
 
+# python web server
+if __name__ == '__main__':
+    app.run(debug=True, port=os.getenv("PORT", default=5000))
